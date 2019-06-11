dfa53b21a4bc7bab96e675a63d48908a6ee2b92f
Diego Camacho <hackvan@gmail.com>
2019-06-11 14:41:55 -0500

    18 - Integracion a Platzi Music

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,59 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    section.section
+      nav.navbar.has-shadow
+        .container
+          .field.has-addons.has-addons-centered
+            .control.is-expanded
+              input.input.is-large(
+                type="text",
+                placeholder="Buscar canciones",
+                v-model="searchQuery"
+                )
+            .control
+              a.button.is-primary.is-large(v-on:click="search") Buscar
+            .control
+              a.button.is-danger.is-large &times;
+          p.help.is-info
+            small {{ searchMessage }}
+
+      .container.results
+        .columns
+          .column(v-for="track in tracks") {{ track.name }} - {{ track.artist }}
 </template>
 
 <script>
+const tracks = [
+  { name: 'Muchacha', artist: 'Luis Alberto Spinetta' },
+  { name: 'Hoy Aca en el baile', artist: 'El pepo' },
+  { name: 'I was made for loving you', artist: 'Kiss' }
+]
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+  methods: {
+    search () {
+      this.tracks = tracks
     }
   }
 }
 </script>
 
 <style lang="scss">
-@import './scss/main.scss'
+  @import './scss/main.scss';
+
+  .results {
+    margin-top: 30px;
+  }
 </style>
