ed92c5a54402c493da6ede791cdfb60c350f52ed
Diego Camacho <hackvan@gmail.com>
2019-06-18 14:35:17 -0500

    24-28 Comunicacion entre componentes padres e hijos

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template lang="pug">
   #app
     pm-header
-    section.section
-      nav.navbar.has-shadow
+
+    pm-loader(v-show="isLoading")
+
+    section.section(v-show="!isLoading")
+      nav.navbar
         .container
           .field.has-addons.has-addons-centered
             .control.is-expanded
               input.input.is-large(
                 type="text",
                 placeholder="Buscar canciones",
-                v-model="searchQuery"
+                v-model="searchQuery",
+                v-on:keyup.enter="search"
                 )
             .control
               a.button.is-primary.is-large(v-on:click="search") Buscar
@@ -20,9 +24,14 @@
           small {{ searchMessage }}
 
       .container.results
-        .columns
-          .column(v-for="track in tracks")
-            | {{ track.name }} - {{ track.artists[0].name }}
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="track in tracks")
+            //- pm-track(v-bind:track="track", v-on:select="selectTrack")
+            //- pm-track(:track="track",       @select="selectTrack")
+            pm-track(
+              :class="{ 'is-active': track.id === selectedTrack }",
+              :track="track",
+              @select="setSelectedTrack")
 
     pm-footer
 </template>
@@ -32,14 +41,19 @@ import trackService from '@/services/track'
 import PmFooter from '@/components/layout/Footer.vue'
 import PmHeader from '@/components/layout/Header.vue'
 
+import PmTrack from '@/components/Track.vue'
+import PmLoader from '@/components/shared/Loader.vue'
+
 export default {
   name: 'app',
 
-  components: { PmFooter, PmHeader },
+  components: { PmFooter, PmHeader, PmTrack, PmLoader },
   data () {
     return {
       searchQuery: '',
-      tracks: []
+      tracks: [],
+      isLoading: false,
+      selectedTrack: ''
     }
   },
   computed: {
@@ -51,10 +65,16 @@ export default {
     search () {
       if (!this.searchQuery) { return }
 
+      this.isLoading = true
+
       trackService.search(this.searchQuery)
         .then(res => {
           this.tracks = res.tracks.items
+          this.isLoading = false
         })
+    },
+    setSelectedTrack (id) {
+      this.selectedTrack = id
     }
   }
 }
@@ -65,5 +85,9 @@ export default {
 
   .results {
     margin-top: 30px;
+  }
+
+  .is-active {
+    border: 3px #23d160 solid;
   }
 </style>
