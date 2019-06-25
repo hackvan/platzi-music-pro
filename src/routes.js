01bbbc6e40df8e95ec58ce61aa82b1508ce076fa
Diego Camacho <hackvan@gmail.com>
2019-06-25 13:37:11 -0500

    33 Agregar la ruta /track/:id y mostrar el componente TrackDetail

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,8 @@ import TrackDetail from '@/components/TrackDetail.vue'
 
 const routes = [
   { path: '/', component: Search, name: 'search' },
-  { path: '/about', component: About, name: 'about' }
+  { path: '/about', component: About, name: 'about' },
+  { path: '/track/:id', component: TrackDetail, name: 'track' }
 ]
 
 export default routes
