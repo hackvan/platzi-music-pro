001cea0f9b73902337a72e8d5cf9f07c2356b83d
Diego Camacho <hackvan@gmail.com>
2019-06-27 13:39:03 -0500

    37 Agregar el mixin para la seleccion del track compartido por el componente Track y TrackDetail

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,28 +1,51 @@
 <template lang="pug">
   .container
     pm-loader(v-show="isLoading")
-    .columns(v-show="!isLoading")
-      .column
-      .column.is-half
-        pm-track(:track="track")
-      .column
+    .columns(v-if="track && track.album" v-show="!isLoading")
+      .column.is-3.has-text-centered
+        figure.media-left
+          p.image
+            img(:src="track.album.images[0].url")
+          p
+            a.button.is-primary.is-large
+              span.icon(@click="selectTrack")
+
+      .column.is-8
+        .panel
+          .panel-heading
+            h1.title {{ track.name }}
+          .panel-block
+            .article.media
+              .media.content
+                .content
+                  ul(v-for="(v, k) in track")
+                    li
+                      strong {{ k }}:&nbsp;
+                      span {{ v }}
+              nav.level
+                .level-left
+                  a.level-item
 </template>
 
 <script>
-import trackService from '@/services/track'
+import trackMixin from '@/mixins/track'
 
-import PmTrack from '@/components/Track.vue'
+import trackService from '@/services/track'
 
 import PmLoader from '@/components/shared/Loader.vue'
 
 export default {
-  components: { PmTrack, PmLoader },
+  components: { PmLoader },
+
+  mixins: [ trackMixin ],
+
   data () {
     return {
       track: {},
       isLoading: false
     }
   },
+
   created () {
     const id = this.$route.params.id
     this.isLoading = true
